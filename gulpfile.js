9ccb619cd91b2d32a1caa719404b02e1af0dc228
Denis Kamensky <kengji45@gmail.com>
2016-11-11 22:29:14 +0300

    first-commit

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var tinypng = require('gulp-tinypng');
+gulp.task('tinypng', function () {
+    gulp.src('src/**/*.*')
+        .pipe(tinypng('77B3MztS7Ebjbv4WY1mApnKUPKRf1Xaa'))
+        .pipe(gulp.dest('compressed_images/'));
+});
