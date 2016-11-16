f7d6f72fbc9afc4595a3a9a8d1cc61a1741f63eb
Denis Kamensky <kengji45@gmail.com>
2016-11-16 17:00:17 +0300

    added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var tinypng = require('gulp-tinypng');
+var clean = require('gulp-clean');
 gulp.task('tinypng', function () {
     gulp.src('src/**/*.*')
-        .pipe(tinypng('77B3MztS7Ebjbv4WY1mApnKUPKRf1Xaa'))
+        .pipe(tinypng('LOS6_ciVHxNcie7NPlwd-xl_vm067sfm'))
         .pipe(gulp.dest('compressed_images/'));
+});
+gulp.task('clean', function () {
+    return gulp.src('src/*', {read: false})
+        .pipe(clean());
 });
 gulp.task('default',function(){
 	gulp.watch('src/**',['tinypng']);
